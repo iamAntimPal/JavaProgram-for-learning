ca43d892612abbc2d6ad7c89f00e6363e115048f
iamAntimPal <antimpal01617121@gmail.com>
2024-05-28 14:19:22 +0530

    awtExample bgcolor

--- AWT/awtExample.java
+++ AWT/awtExample.java
@@ -3,8 +3,9 @@ public class awtExample extends Frame{
      awtExample()
      {
           Button b = new Button("Click me ...!");
-          b.setBounds(30,20,100,30);
+          b.setBounds(30,40,100,30);
           add(b);
+          b.setBackground(Color.BLUE);
           setSize(200,200);
           setTitle("This is my example");
           setLayout(null);
